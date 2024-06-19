f6be0562d89cceaa6df710ed27d5e92ce6625018
jabss <jabrijuhinin93@gmail.com>
2024-06-20 05:40:05 +0800

    all test pass. exercise 4.3-4.7 completed

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -11,18 +11,68 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-    const listWithOneBlog = [
-      {
-        "author": "HOREYharahereyy",
-        "url": "www.uploadtestffortodayy.com",
-        "likes": 20,
-        "title": "testtukarnama",
-        "id": "6672f1a6551c99e7351ebfa9"
-      }
+    const listWithMoreThanOneBlog = [
+        {
+            "author": "JABSS",
+            "url": "www.testuploadbloggg.com",
+            "likes": 3,
+            "title": "test lagi 3231321321",
+            "id": "6671b7adf198a684059c9f07"
+        },
+        {
+            "author": "HOREYharahereyy",
+            "url": "www.uploadtestffortodayy.com",
+            "likes": 20,
+            "title": "testtukarnama",
+            "id": "6672f1a6551c99e7351ebfa9"
+        },
+        {
+            "title": "testlagilagi",
+            "author": "jabsszsz",
+            "url": "www.eqweqw123.com",
+            "likes": 6,
+            "id": "6672fcaecc5aae16ed38a083"
+        },
+        {
+            "title": "test lagi add",
+            "author": "test add dluuu",
+            "url": "www.testaddlagi.com",
+            "likes": 13,
+            "id": "6672fd98cc5aae16ed38a08a"
+        },
+        {
+            "title": "test lagi 3231321321",
+            "author": "jabs",
+            "url": "www.fullstackopen-part4.com",
+            "likes": 30,
+            "id": "6672ff67cc5aae16ed38a09c"
+        }
     ]
+
+    const listWithOneBlog = [
+        {
+            "author": "JABSS",
+            "url": "www.testuploadbloggg.com",
+            "likes": 3,
+            "title": "test lagi 3231321321",
+            "id": "6671b7adf198a684059c9f07"
+        }
+    ]
+
+    const emptyList = [];
+
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes(emptyList)
+        assert.strictEqual(result, 0)
+    })
   
     test('when list has only one blog, equals the likes of that', () => {
       const result = listHelper.totalLikes(listWithOneBlog)
-      assert.strictEqual(result, 20)
+      assert.strictEqual(result, 3)
     })
-  })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithMoreThanOneBlog)
+        assert.strictEqual(result, 72)
+      })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,10 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
 
-  const totalLikes = (blogs) => {
+const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
-  };
+};
 // const totalLikes = (blogs) => {
 //     return blogs.likes === totalLikes.likes 
 
