5acf9143753db8447d1a03d4eac8bb891a1423f6
jabss <jabrijuhinin93@gmail.com>
2024-06-20 05:08:03 +0800

    totalLikes test working

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,6 +1,7 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const totalLikes = require('../utils/list_helper').totalLikes
 
 test('dummy returns one', () => {
   const blogs = []
@@ -8,3 +9,20 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        "author": "HOREYharahereyy",
+        "url": "www.uploadtestffortodayy.com",
+        "likes": 20,
+        "title": "testtukarnama",
+        "id": "6672f1a6551c99e7351ebfa9"
+      }
+    ]
+  
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      assert.strictEqual(result, 20)
+    })
+  })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,27 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  };
+// const totalLikes = (blogs) => {
+//     return blogs.likes === totalLikes.likes 
+
+//     console.log(onlyLike)
+//     // const blogList = blog.map()
+//     // const reducer = (sum, item) => {
+//     //   return sum + item
+//     // }
+//     // return array.length === 0
+//     // ? 0
+//     // : array.reduce(reducer, 0) / array.length
+// }
+
+// totalLikes();
+
+  
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
