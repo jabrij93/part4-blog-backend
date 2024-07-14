c63ffea0ff42262973a97e8f94975466cd928a4d
jabss <jabrijuhinin93@gmail.com>
2024-07-14 19:15:32 +0800

    refactor get token function (exercises 4.20* completed)

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,16 @@
 const logger = require('./logger')
 
+// Get Token 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null;
+  }
+  next()
+}
+
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
@@ -31,6 +42,7 @@ const errorHandler = (error, request, response, next) => {
 }
 
 module.exports = {
+  tokenExtractor,
   requestLogger,
   unknownEndpoint,
   errorHandler
