1f5d8b6ca9ce184ea68c1a7deaf4a7eb8a5e2272
jabss <jabrijuhinin93@gmail.com>
2024-07-17 06:03:14 +0800

    add new blog post with refactor code working(this and previous commit means exercise 4.22 completed)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -28,14 +28,10 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   if (!body.title || !body.url) {
     return response.status(400).json({
