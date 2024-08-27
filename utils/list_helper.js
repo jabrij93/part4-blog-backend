ba145573e24bd562bd0977d1c82896873417a0d7
jabss <jabrijuhinin93@gmail.com>
2024-08-27 23:16:29 +0800

    cleanup

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,24 +5,7 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
-
-// const totalLikes = (blogs) => {
-//     return blogs.likes === totalLikes.likes 
-
-//     console.log(onlyLike)
-//     // const blogList = blog.map()
-//     // const reducer = (sum, item) => {
-//     //   return sum + item
-//     // }
-//     // return array.length === 0
-//     // ? 0
-//     // : array.reduce(reducer, 0) / array.length
-// }
-
-// totalLikes();
-
   
-  
-  module.exports = {
-    dummy, totalLikes
-  }
+module.exports = {
+  dummy, totalLikes
+}
